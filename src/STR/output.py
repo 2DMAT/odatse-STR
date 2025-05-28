c4025d0d22d76467f5a9618325d15968db448e01
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2025-05-28 16:33:34 +0900

    fix index

--- src/STR/output.py
+++ src/STR/output.py
@@ -128,10 +128,10 @@ class Output(object):
 
         # Normalization of reference data
         if self.normalization == "TOTAL":
-            data = data_experiment[:, cal_number[0]]
+            data = data_experiment[:, self.exp_number[0]]
             norm = np.sum(data)
             data_normalized = data / norm
-            self.I_reference_normalized_l = data_normalized.transpose()
+            self.I_reference_normalized_l = data_normalized.reshape(1,-1)
 
         elif self.normalization == "MANY_BEAM":
             data = data_experiment[:,self.exp_number]
@@ -467,7 +467,7 @@ class Output(object):
         if self.normalization == "TOTAL":
             data = data_convolution[:, cal_number[0]]
             norm = np.sum(data)
-            data_normalized = (data / norm).transpose()
+            data_normalized = (data / norm).reshape(1,-1)
 
         elif self.normalization == "MANY_BEAM":
             data = data_convolution[:, cal_number]
